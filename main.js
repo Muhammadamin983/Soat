5b86563b55d4ac8b5fccec20a62b4dce7a5ead27
Madaminjon <madaminjonromonov82@gmail.com>
2025-11-21 17:13:26 +0500

    Qoshimchalar

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 let s = document.querySelector(".s")
 let m = document.querySelector(".m")
 let h = document.querySelector(".h")
+let hours = document.querySelector('.hours')
+let minutes = document.querySelector('.minutes')
 
 function clock() {
     let data = new Date()
@@ -16,6 +18,29 @@ function clock() {
     setTimeout(() => {
         clock()
     }, 1000);
+
+    hours.innerHTML = data.getHours() < 10 ? '0'+data.getHours() : data.getHours()
+    minutes.innerHTML = data.getMinutes() < 10 ? '0'+data.getMinutes() : data.getMinutes()
 }
 
 clock()
+
+let tabsItem = document.querySelectorAll('.tabsItem')
+let tabsContentItem = document.querySelectorAll('.tabsContentItem')
+
+for (let i = 0; i < tabsItem.length ; i++) {
+    
+    tabsItem[i].addEventListener('click' , function () {
+
+       for (let k =0 ; k < tabsItem.length ; k++) {
+
+        tabsItem[k].classList.remove('active')
+        tabsContentItem[k].classList.remove('active')
+        
+       }
+
+       tabsItem[i].classList.add('active')
+       tabsContentItem[i].classList.add('active')
+        
+    })    
+}
